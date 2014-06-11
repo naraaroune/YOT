fb65883bbf5ad0871d9ac0c5ade8a2be4dd9acc2
naraaroune <ronaldo89>
2014-06-11 23:24:13 +0200

    v1

---
+++ YOT.Test/RunnerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace YOT.Test
+{
+    [TestFixture]
+    public class RunnerTest {
+
+        private Runner runner= new Runner();
+
+        [Test]
+        public void Should_Load_Assembly() {
+            Assert.IsTrue(runner.LoadDll("YOT.dll"));
+            Assert.IsFalse(runner.LoadDll("YOTFAIL.dll"));
+        }
+
+    }
+}
---
+++ YOT/Runner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOT
+{
+    public class Runner
+    {
+        private Assembly Dll;
+
+        public bool LoadDll(string pathdll)
+		{
+            try 
+			{
+                if (!File.Exists(pathdll) || pathdll == null) 
+                    return false;
+
+                Dll = Assembly.LoadFrom(pathdll);
+                return true;
+            }
+            catch (Exception ex) 
+			{
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
